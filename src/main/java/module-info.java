1be8967bc29b37e4004cd7bfc5c1327c5936deab
xKyao <geiger_jonas@web.de>
2026-05-11 23:40:39 +0200

    api import vorbereiten, doin not yet

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module net.sympia.minion1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
 
 
     opens net.sympia.minion1 to javafx.fxml;
     exports net.sympia.minion1;
+    exports net.sympia.minion1.api;
 }
---
+++ src/main/java/net/sympia/minion1/api/ApiClient.java
@@ -0,0 +1,10 @@
+package net.sympia.minion1.api;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiClient {
+}
